ba6cc026600502d452e30d118fa6bd16e5aa1cc7
26watsonkl <142427575+26watsonkl@users.noreply.github.com>
2024-05-06 09:53:49 -0500

    Add files via upload

---
+++ ItemDB.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> itemDB = new List<Item>();
+    void Start()
+    {
+        itemDB[1,].Action();
+    }
+
+    
+}
